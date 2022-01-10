a0a54e0ea2680d4db5e5db86ca2a0a0f6252bd51
Guilherme <g.fernandes2223@gmail.com>
2022-01-10 15:10:58 -0300

    Corringindo erros

--- script.js
+++ script.js
@@ -4,13 +4,38 @@ button.addEventListener('click', event => {
   event.preventDefault()
   const name = document.getElementById('name')
   const email = document.getElementById('email')
+  const spanErrorName = document.getElementById('spanErrorName')
+  const spanErrorEmail = document.getElementById('spanErrorEmail')
+
+  if (name.value == '') {
+    spanErrorName.style.display = 'block'
+  } else {
+    spanErrorName.style.display = 'none'
+  }
+
+  if (email.value == '') {
+    spanErrorEmail.style.display = 'block'
+  } else {
+    spanErrorEmail.style.display = 'none'
+  }
 
   if (name.value == '') {
     name.classList.add('errorInput')
   } else {
     name.classList.remove('errorInput')
   }
+
   if (name.value == '') {
+    email.classList.add('errorInput')
+  } else {
+    email.classList.remove('errorInput')
+  }
+
+  if (
+    email.value.indexOf('@') == -1 ||
+    email.value.indexOf('.') == -1 ||
+    (email.value.indexOf('.') == 1 - email.value.indexOf('@')) == 1
+  ) {
     email.classList.add('errorInput')
   } else {
     email.classList.remove('errorInput')
@@ -24,7 +49,4 @@ function validation() {
   const email = document.getElementById('email').value
 
   console.log('Nome:', name, 'Email:', email)
-
-  if (name === '') alert('Fill in the blank field!')
-  if (email === '') alert('Fill in the blank field!')
 }
